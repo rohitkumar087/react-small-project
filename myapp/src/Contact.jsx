e55368b814b7238959dfa5491f67fe29b96f4de7
rohit kumar <rohitkumarprajapat087@gmail.com>
2025-07-22 19:20:57 +0530

    created full portfolio with live link

--- myapp/src/Contact.jsx
+++ myapp/src/Contact.jsx
@@ -9,8 +9,11 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <p><b><i class="fa-solid fa-envelope"></i> Email :</b>  rohitkumar087@gmail.com</p>
       <p><b><i class="fa-solid fa-phone"></i> Mobile :</b> +91-9320618432</p>
-      <a href='https://github.com/rohitkumar087' target='blank' className='btn' style={{marginRight:"30px"}}>Github <i class="fa-brands fa-github"></i></a>
-      <a href='https://www.linkedin.com/in/rohit-kumar-8567a8314?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BXxNxuf%2FqTZmIy8f%2F%2F6w7zg%3D%3D'  target='blank' className='btn'>Linkedin <i class="fa-brands fa-linkedin"></i></a>
+      <div className='mediaPosition'>
+      <a href='https://github.com/rohitkumar087' target='blank' className='btn' style={{marginRight:"30px"}}>Github  <i class="fa-brands fa-github"></i></a>
+      <a href='https://www.linkedin.com/in/rohit-kumar-8567a8314?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BXxNxuf%2FqTZmIy8f%2F%2F6w7zg%3D%3D' target='blank' className='btn'>Linkedin <i class="fa-brands fa-linkedin"></i></a>
+
+      </div>
     </div>
     </>
   );
--- myapp/src/Home.jsx
+++ myapp/src/Home.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import './index.css'
 import Nav from "./nav";
 import profileImage from './assets/Profile.jpg'
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    
+
     <>
-    <Nav/>
-    <div className="page home">
-      <img src={profileImage} alt="Profile image" style={{height:"300px",width:"300px", borderRadius:"50%"}}/>
-      <div style={{position:"relative", bottom:"40px"}}><h1>Rohit Kumar</h1>
-      <p>Full Stack Java Developer</p>
-      <a href="/projects" className="btn">View Projects</a>
-    </div></div>
+      <Nav />
+      <div className="page home">
+        <div className="imgPosition">
+          <img src={profileImage} alt="Profile image" style={{ height: "300px", width: "300px", borderRadius: "50%" }} />
+
+        </div>
+        <div style={{ position: "relative", bottom: "40px" }}><h1>Rohit Kumar</h1>
+          <p>Full Stack Java Developer</p>
+          <div className="btnPosition">
+            <Link to={'/projects'} className="btn">View Projects</Link>
+          </div>
+        </div></div>
     </>
   );
 };
